5e5839227b5e0d6879e738b7b0e88055bb6bc4d9
oskitar92 <130413246+oskitar92@users.noreply.github.com>
2024-06-13 01:57:19 +0200

    Terminado

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
  // Aquí tu código
-1
+ const boton = document.getElementById("agregar");
+ const lista = document.getElementById("lista");
+ 
+ boton.addEventListener("click", function() {
+   const nuevoElemento = prompt("Introduce un nuevo elemento:");
+   if (nuevoElemento) {
+     const li = document.createElement('li'); 
+     li.textContent = nuevoElemento;
+     lista.appendChild(li);
+   }
+ });
